c0832f400f52d99a6f7dab417511217745e56f9f
ephilbin <143443264+ephilbin@users.noreply.github.com>
2023-10-18 14:42:46 -0400

    Add files via upload

---
+++ name.py
@@ -0,0 +1,8 @@
+#Ask user for their name
+name = input("What's your name?").strip().title()
+
+#remove whitespace from their name by using .strip()
+
+#capitalize user's name by using .title() 
+
+print(f"hello, {name}")
